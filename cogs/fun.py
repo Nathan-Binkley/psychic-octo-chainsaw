5d476221569eefb1505c1e00fd62a8239c4fd319
Nathan-Binkley <nbink2@gmail.com>
2022-01-10 18:33:28 -0500

    fun insult bot

--- cogs/fun.py
+++ cogs/fun.py
@@ -1,5 +1,6 @@
 from discord.ext import commands
 import discord
+import random
 
 import sys
 sys.dont_write_bytecode = True
@@ -8,7 +9,8 @@ class fun(commands.Cog):
 
     def __init__(self, bot):
         self.bot = bot
-    
+        self.insults = self.load_insults()
+
     @commands.command()
     async def bubblewrap(self, ctx):
 
@@ -19,6 +21,20 @@ class fun(commands.Cog):
             msg += '\n'
 
         await ctx.send(msg)
+
+    def load_insults(self):
+        with open("cogs/data/insults.txt", "r") as f:
+            return f.read().splitlines() #I don't know if this will work lmao
+
+    @commands.command()
+    async def insult(self, ctx, user = None):
+        if not user:
+            msg = self.insults[random.randInt(len(self.insults))]
+        else:
+            msg = f"Hey {user}! {self.insults[random.randInt(len(self.insults))]}" #I don't know how well this will work either lol
+        await ctx.send(msg)
+
+    
 
 def setup(bot):
     bot.add_cog(fun(bot))
