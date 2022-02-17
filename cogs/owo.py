c19e9b64283b2576f77f0243cebc78a39d1bcb2a
Nathan-Binkley <nbink2@gmail.com>
2022-02-16 22:13:28 -0500

    Fixed issue with Discord's handling of quotations in relation to owo. See owo cog for details'

--- cogs/owo.py
+++ cogs/owo.py
@@ -9,23 +9,28 @@ class owo(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
         self.filterList = ['://','www.','.com','.net','.gov','.org','https','http', '@', '#', 'RT']
-        self.customEndings = ['( ͡° ͜ʖ ͡°) ',"*nuzzles you* OwO ", "*Pounces on Daddy's lap* UwU What's this? ",  "*stares deep into your eyes* I wuv you ", "*Kisses you* ", "º꒳º ", "Pwease Daddy? I can be youw pwincess (⑅˘꒳˘) " ,"OwO *notices bulge* ", "ouo ", "rawr XD ", "owo ", "UwU ", "*Softly pets your cute head* ", "Do you need some nuzzle wuzzle?"]
+        self.customEndings = ['( ͡° ͜ʖ ͡°) ',"*nuzzles you* OwO ", "*Pounces on Daddy's lap* UwU What\'s this? ",  "*stares deep into your eyes* I wuv you ", "*Kisses you* ", "º꒳º ", "Pwease Daddy? I can be youw pwincess (⑅˘꒳˘) " ,"OwO *notices bulge* ", "ouo ", "rawr XD ", "owo ", "UwU ", "*Softly pets your cute head* ", "Do you need some nuzzle wuzzle?"]
         self.customBeginnings = ['Mommy ', '*Stares into your eyes and says* ', '( ͡° ͜ʖ ͡°) ', '*purrs on your lap* ', '*Jumps on Daddy\'s lap* ']
 
     @commands.command()
-    async def owo(self, ctx, *arg):
+    async def owo(self, ctx, *, arg):
         owod = ""
+        
         text = ctx.message.content.split(" ", 1)[1]
+        text = text.replace("'", "\'")
         texts = text.split(" ") #Split on spaces
+        
         for i in texts: #for each item in the split list
             i = i.rstrip()  #remove \n characters 
         
             temp = False 
+
             for j in self.filterList: #allows filtering based on predetermined list
                 if j in i:  # if filter index in the original word
                     owod += i   #ignore it
                     temp = True
                     break
+
             if not temp:    # if filter index not in word
                 for j in i: # go through each letter
                     if j == "l" or j == "r":    # replace
